f208f486802c7af8bfd614e745ec63189509e6c8
lefetz1 <sacha.lefetz@etu.unilim.fr>
2023-02-07 15:20:15 +0100

    3eme commit

--- Front-V1/src/main.js
+++ Front-V1/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Header from './Header.vue'
 
 import './assets/style.css'
 
 createApp(App).mount('#app')
+createApp(Header).mount('#header')
