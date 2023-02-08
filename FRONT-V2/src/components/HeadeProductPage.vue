c9098274f4bba38f7fc2c4c41f2482efb22bb2de
lefetz1 <sacha.lefetz@etu.unilim.fr>
2023-02-08 14:57:59 +0100

    Page des catégories

---
+++ FRONT-V2/src/components/HeadeProductPage.vue
@@ -0,0 +1,58 @@
+<script>
+    export default {
+        props: {
+            oneProduct : {
+                type: Array,
+                required: true,
+                validator(value) {
+                    return value.length <= 100;
+                }
+            },
+        }
+    }
+</script>
+<template>
+    <div class="product">
+        <img class="product--img" src="/assets/products/polos-original.png" alt="#">
+        <div class="article">
+            <p class="article--title">{{oneProduct.name}}</p>
+            <div class="article__evaluation">
+                <p class="article__evaluation--price">{{oneProduct.price}}</p>
+                <div class="article__evaluation__rating">
+                    <img class="article__evaluation__rating--star" src="/assets/icons/star.svg" alt="#">
+                    <img class="article__evaluation__rating--star" src="/assets/icons/star.svg" alt="#">
+                    <img class="article__evaluation__rating--star" src="/assets/icons/star.svg" alt="#">
+                    <img class="article__evaluation__rating--star" src="/assets/icons/star.svg" alt="#">
+                    <img class="article__evaluation__rating--star" src="/assets/icons/star.svg" alt="#">
+                </div>
+            </div>
+            <!-- <div class="article__options">
+                <p class="article__options--title">Colors</p>
+                <div class="article__options__choice">
+                    <button class="article__options__choice--btn btncolors"></button>
+                    <button class="article__options__choice--btn btncolors"></button>
+                    <button class="article__options__choice--btn btncolors"></button>
+                    <button class="article__options__choice--btn btncolors"></button>
+                    <button class="article__options__choice--btn btncolors"></button>
+                    <button class="article__options__choice--btn btncolors"></button>
+                </div>
+            </div>
+            <div class="article__options">
+                <p class="article__options--title">Sizes</p>
+                <div class="article__options__choice">
+                    <button class="article__options__choice--btn btnsizes">S</button>
+                    <button class="article__options__choice--btn btnsizes">M</button>
+                    <button class="article__options__choice--btn btnsizes">L</button>
+                    <button class="article__options__choice--btn btnsizes">XL</button>
+                </div>
+            </div> -->
+            <div class="article__options">
+                <p class="article__options--title">Description</p>
+                <p class="article__options--txt">{{oneProduct.description}}</p>
+            </div>
+            <button class="article--cart">Click & Collect (Retrait en Magasin)</button>
+
+        </div>
+
+    </div>
+</template>
