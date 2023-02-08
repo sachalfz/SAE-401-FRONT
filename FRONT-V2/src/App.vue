3c590ea82cb97786f32c844c208ecc3f65cf51b7
lefetz1 <sacha.lefetz@etu.unilim.fr>
2023-02-08 12:06:30 +0100

    routes

---
+++ FRONT-V2/src/App.vue
@@ -0,0 +1,66 @@
+<script >
+import HeaderComponent from './components/HeaderComponent.vue';
+import Banner from './components/Banner.vue';
+import ProductList from './components/ProductList.vue';
+import Homepage from './components/Homepage.vue';
+
+export default {
+  components :{
+    HeaderComponent,
+    Banner,
+    Homepage,
+    ProductList
+  },
+  data() {
+    return {
+      cat:  [
+              {id: 1, name: "Polos"}, 
+              {id: 2, name: "Sneakers"}, 
+              {id: 3, name: "Pants"}
+      ],
+
+      products: [
+      {
+        id: 1,
+        name: "Un beau polo",
+        description: "edqnjgbipb",
+        price: 160,
+        img: "https://picsum.photos/400"
+      },
+      {
+        id: 1,
+        name: "Un beau polo",
+        description: "edqnjgbipb",
+        price: 160,
+        img: "https://picsum.photos/400"
+      },
+      {
+        id: 1,
+        name: "Un beau polo",
+        description: "edqnjgbipb",
+        price: 160,
+        img: "https://picsum.photos/400"
+      },
+      {
+        id: 1,
+        name: "Un beau polo",
+        description: "edqnjgbipb",
+        price: 160,
+        img: "https://picsum.photos/400"
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+    <HeaderComponent v-bind:categories="cat"></HeaderComponent>
+    <main class="main">
+      <router-view v-bind:listProduct="products"/> 
+    </main> 
+</template>
+
+<style>
+
+</style>
