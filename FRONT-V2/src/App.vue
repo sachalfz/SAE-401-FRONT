054f956a129fc5349d73e83e37e90a687a050e40
Sacha Lefetz <sacha.lefetz@gmail.com>
2023-02-24 10:18:39 +0100

    liason bdd externe

--- FRONT-V2/src/App.vue
+++ FRONT-V2/src/App.vue
@@ -4,7 +4,7 @@ import Banner from './components/Banner.vue';
 import ProductList from './components/ProductList.vue';
 import Homepage from './components/Homepage.vue';
 
-const baseURL = 'http://127.0.0.1:8000/api/';
+const baseURL = 'https://mmi.unilim.fr/~maury92/SAE-401-Back%20-%20RedoEasyAdmin/public/index.php/api/';
 
 export default {
   components :{
@@ -15,58 +15,6 @@ export default {
   },
   data() {
     return {
-      // categories: [
-      //   {name: 'Polos'},
-      //   {name: 'Jeans'},
-      //   {name: 'Sneakers'}
-      // ],
-
-      // oneCat:  {
-      //   description: "Pantalons de très bonne qualité", 
-      //   name: "Pants",
-      //   img: "https://picsum.photos/1500/1500"
-      // },
-
-      // oneProduct:  {
-      //   id: 1,
-      //   name: "Un beau polo",
-      //   description: "edqnjgbipb",
-      //   price: 160,
-      //   img: "https://picsum.photos/400"
-      // },
-
-      // products: [
-      // {
-      //   id: 2,
-      //   name: "Un beau polo",
-      //   description: "edqnjgbipb",
-      //   price: 160,
-      //   img: "https://picsum.photos/400"
-      // },
-      // {
-      //   id: 3,
-      //   name: "Un beau polo",
-      //   description: "edqnjgbipb",
-      //   price: 160,
-      //   img: "https://picsum.photos/400"
-      // },
-      // {
-      //   id: 4,
-      //   name: "Un beau polo",
-      //   description: "edqnjgbipb",
-      //   price: 160,
-      //   img: "https://picsum.photos/400"
-      // },
-      // {
-      //   id: 5,
-      //   name: "Un beau polo",
-      //   description: "edqnjgbipb",
-      //   price: 160,
-      //   img: "https://picsum.photos/400"
-      // }
-      // ]
-    
-
       categories:  [],
 
       oneCat:  {},
